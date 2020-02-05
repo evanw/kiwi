33dbd7a1f98173f59b0c166ee525d49d5bf6d50c
Evan Wallace <evan.exe@gmail.com>
2020-02-04 17:14:15 -0800

    add a minified binary-only browser file

---
+++ js/browser.ts
@@ -0,0 +1,9 @@
+// This is a small entry point intended for use in the browser
+
+import { decodeBinarySchema } from "./binary";
+import { compileSchema } from "./js";
+
+export {
+  decodeBinarySchema,
+  compileSchema,
+}
